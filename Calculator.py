190e84f34fc32fb00936f8595c733b1ce08d4a51
crm0023 <130826166+crm16@users.noreply.github.com>
2025-03-31 10:17:05 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,20 @@
+
+no1 = float(input("Enter first number: "))
+op = input("Enter arithmetic operator : ")
+no2 = float(input("Enter second number: "))
+
+
+if op == "+":
+    print(no1 + no2)
+
+elif op == "-":
+    print(no1 - no2)
+
+elif op == "*":
+    print(no1 * no2)
+
+elif op == "/":
+    print(no1/no2)
+
+else:
+    print("Invalid choice....")
